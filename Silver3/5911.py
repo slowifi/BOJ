1149ac9232499063cad464fc4f3e0b2d86bc4704
slowifi <okc05181@gmail.com>
2025-04-12 20:21:29 +0900

    Marathon end.

--- Silver3/5911.py
+++ Silver3/5911.py
@@ -1,10 +1,39 @@
 import sys
 
-N, B = map(int, sys.stdin.readline().strip().split(' '))
+N, M = map(int, sys.stdin.readline().strip().split(' '))
 
-P = []
-S = []
+Arr1 = []
+Arr2 = []
+ret = []
+
 for _ in range(N):
     A, B = map(int, sys.stdin.readline().strip().split(' '))
-    P.append(A)
-    S.append(B)
+    Arr1.append(A)
+    Arr2.append(B)
+    
+
+for i in range(N):
+    data = []
+    count = 0
+    s = 0
+
+    for j in range(N):
+        if i == j:
+            data.append(Arr1[j] // 2 + Arr2[j])
+        else:
+            data.append(Arr1[j] + Arr2[j])
+    
+    data.sort()
+    #print(data)
+
+    for j in range(N):
+        s += data[j]
+        if s > M:
+            break
+        count += 1
+    ret.append(count)   
+    #print(ret)
+print(max(ret))
+
+
+        
--- Sliver5/12509.py
+++ Sliver5/12509.py
@@ -12,17 +12,11 @@ for j in range(n):
     Pos_B = 1
     for i in range(int(data[0])):
         if data[i*2 + 1] == "O":
-
-            Time_O = abs(int(data[i*2 + 2]) - Pos_O) + 1
-            if Time < Time_O:
-                Time = Time_O
-
-            
+            Time_O = max(Time_O + abs(int(data[i*2 + 2]) - Pos_O), Time_B) + 1
+            Pos_O = int(data[i*2 + 2])
         else:
-            if Time_O > Time_B:
-                Time_B = Time_O + abs(int(data[i*2 + 2]) - Pos_B) + 1
-            else:
-                Time_B += abs(int(data[i*2 + 2]) - Pos_B) + 1
-        print(Time_B, Time_O)
-    print("Case #%d: " %j, end="")
+            Time_B = max(Time_B + abs(int(data[i*2 + 2]) - Pos_B), Time_O) + 1
+            Pos_B = int(data[i*2 + 2])
+        # print(Time_B, Time_O)
+    print("Case #%d: " %(j+1), end="")
     print(max(Time_O, Time_B))
